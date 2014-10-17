f5c451d6bc617c29e2c103039216492cb7a91062
Mike James <mike@xamarin.com>
2014-10-17 18:04:24 +0100

    Initial commit
    Fetch an NSImage asynchronously
    Check if the app installed in the Applications directory?
    Fetch the OS version
    Check if app is on a retina device

---
+++ CocoaExtensions/CocoaExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using MonoMac.AppKit;
+using System.Threading.Tasks;
+using MonoMac.Foundation;
+
+namespace CocoaExtensions
+{
+    public static class CocoaExtensions
+    {
+        public static bool IsRetina
+        {
+            get
+            {
+                return NSScreen.MainScreen.BackingScaleFactor == 2.0f;
+            }
+        }
+
+        async public static Task<NSImage> DownloadImage(string url)
+        {
+            var webclient = new System.Net.WebClient();
+            var imageBytes = await webclient.DownloadDataTaskAsync(new Uri(url));
+            NSImage image = null;
+            try 
+            {
+                image = new NSImage(NSData.FromArray(imageBytes));
+                imageBytes = null;
+            } 
+            catch (Exception ) 
+            {
+                return null;
+            }
+            return image;
+        }
+
+        public static string OSVersion
+        {
+            get
+            {
+                var info = new NSProcessInfo();
+                return info.OperatingSystemVersionString;
+            }
+        }
+
+        public static string OSName
+        {
+            get
+            {
+                var info = new NSProcessInfo();
+                return info.OperatingSystemName;
+            }
+        }
+
+        public static bool IsInstalledToApplications
+        {
+            get
+            {
+                var path = NSBundle.MainBundle.BundlePath;
+                Console.WriteLine(path);
+                return path.Contains("Applications");
+            }
+
+        }
+    }
+}
+
